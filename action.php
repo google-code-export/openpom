3120b935381b1f1f3e28406d72f88213d10146bb
sylsmu <sylsmu@gmail.com>
2010-10-04 20:41:20 +0000

    table action (ie bug)

--- action.php
+++ action.php
@@ -13,25 +13,16 @@
   if (!isset($_SESSION['USER'])) die(); ?>
 
   <form action="index.php<?php echo $MY_GET?>" method="post" name="action" />
-    <a class="ack" href="#" id="acklink"><input type="button" name="ack"
-      value="<?php echo ucfirst(lang($MYLANG, 'acknowledge'))?>" /></a>
-
-    <a class="down" href="#" id="downlink"><input type="button" name="down"
-      value="<?php echo ucfirst(lang($MYLANG, 'downtime'))?>" /></a>
-
-    <input type="submit" name="recheck"
-      value="<?php echo ucfirst(lang($MYLANG, 'recheck'))?>" />
-
-    <input type="submit" name="disable"
-      value="<?php echo ucfirst(lang($MYLANG, 'disable'))?>" 
-      onClick="EvalSound('sound1')" title="<?php echo ucfirst(lang($MYLANG, 'disable_title'))?>" />
-
-    <input type="submit" name="reset"
-      value="<?php echo ucfirst(lang($MYLANG, 'reset'))?>" title="<?php echo ucfirst(lang($MYLANG, 'reset_title'))?>" />
-
-    <a class="comment" href="#" id="commentlink"><input type="button" name="comment"
-      value="<?php echo ucfirst(lang($MYLANG, 'comment0'))?>" /></a>
-
-    <?php if (isset($LOG)) { ?>
-    <div style="display:inline; float:right; margin:0; padding:5px 0 0 0; text-align:right;"><a href="#" onClick='pop("<?php echo $LOG; ?>","nagios_log",600,400);'><?php echo ucfirst(lang($MYLANG, 'show_log'))?></a></div>
-    <?php } ?>
+  <table class="action">
+    <tr>
+      <td><a class="ack" href="#" id="acklink"><input type="button" name="ack" value="<?php echo ucfirst(lang($MYLANG, 'acknowledge'))?>" /></a></td>
+      <td><a class="down" href="#" id="downlink"><input type="button" name="down" value="<?php echo ucfirst(lang($MYLANG, 'downtime'))?>" /></a></td>
+      <td><input type="submit" name="recheck" value="<?php echo ucfirst(lang($MYLANG, 'recheck'))?>" /></td>
+      <td><input type="submit" name="disable" value="<?php echo ucfirst(lang($MYLANG, 'disable'))?>" onClick="EvalSound('sound1')" title="<?php echo ucfirst(lang($MYLANG, 'disable_title'))?>" /></td>
+      <td><input type="submit" name="reset" value="<?php echo ucfirst(lang($MYLANG, 'reset'))?>" title="<?php echo ucfirst(lang($MYLANG, 'reset_title'))?>" /></td>
+      <td><a class="comment" href="#" id="commentlink"><input type="button" name="comment" value="<?php echo ucfirst(lang($MYLANG, 'comment0'))?>" /></a></td>
+      <?php if (isset($LOG)) { ?>
+      <td style="text-align:right; width:100%;"><a href="#" onClick='pop("<?php echo $LOG; ?>","nagios_log",600,500);'><?php echo ucfirst(lang($MYLANG, 'show_log'))?></a></td>
+      <?php } ?>
+    </tr>
+  </table>
