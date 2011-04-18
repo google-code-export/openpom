3aad5d70d9fe1544d8e92cb0fc6bcad2311f5fab
omk8421 <omk8421@gmail.com>
2011-04-18 09:43:57 +0000

    Fix typo in HTML doctype

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 */
  
   if (basename($_SERVER['SCRIPT_NAME']) != "index.php") die() ; ?>
-<!DCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">                                                                  
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">                                                                  
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
   <head>
     <title>
