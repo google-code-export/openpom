d8093ac11c36eac360995a9d3ec8af8062715bc8
omk8421 <omk8421@gmail.com>
2011-04-18 11:57:06 +0000

    Move the shift-click multiselect feature into the rowclick event handler

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,12 @@
 $(document).ready(function(){
-  $(".ack,.down,.option,.comment").colorbox({
-    initialWidth: "340px",
-    initialHeight: "340px"	
-  });
-  $('.chkbox').click(function(event) {
-    if(!lastChecked) {
-      lastChecked = this;
-      return;
-    }
-    if(event.shiftKey) {
-      var startchkbox = $('.chkbox').index(this);
-      var endchkbox = $('.chkbox').index(lastChecked);
-      for(i=Math.min(startchkbox,endchkbox);i<Math.max(startchkbox,endchkbox);i++) {
-        $('.chkbox')[i].checked = lastChecked.checked;
-      }
-    }
-    lastChecked = this;
-  });
-  
+
+  /* put the status popin at right place */
   var first_alert_item = $('table#alert tr.alert-item:first');
   if (first_alert_item.length) {
     popup.css('top', (first_alert_item.offset().top - 10) + 'px');
   }
   
+  /* add the status popin to document */
   $('body').append(popup);
 });
 
