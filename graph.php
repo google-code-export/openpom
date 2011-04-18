af9136ee57cdde8866709c2ea949ad511bccb79a
omk8421 <omk8421@gmail.com>
2011-04-18 13:37:36 +0000

    Fix typo in graph popup' doctype

--- graph.php
+++ graph.php
@@ -56,7 +56,7 @@ $graph .= '&t1=' . strtotime('-1 ' . $period);
 $graph .= '&t2=' . time();
 
 ?>
-<!DCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
   <head>
   <title><?php echo $host; if (!is_null($svc)) { echo " &#160;&mdash;&#160; ".$svc; } ?></title>
