5921821fb47778b20fc5495460649eb4e9e2a294
omk8421 <omk8421@gmail.com>
2011-04-18 12:01:28 +0000

    Rebuild alert listing using new fake cross-browser checkboxes

--- alert.php
+++ alert.php
@@ -14,17 +14,34 @@
   if (!isset($_SESSION['USER'])) die();
 ?>
 
-    <table width="100%" class="alert" id="alert">
-      <tr class="headers">
+
+
+    <style type="text/css">
+      table#alert * {
+        font-size: <?= $_SESSION['FONTSIZE'] ?>px;
+      }
+    </style>
+    
+    
+    <?php
+      if (!isset($_GET['monitor'])) {
+        echo '<div style="height: 39px; width: 1px;"></div>';
+      } 
+    ?>
+    
+    <table width="100%" id="alert">
+      <tr>
         <?php 
           foreach($COLS AS $key => $val) { 
             if ($key == 'checkbox') {
         ?>
         <th class="checkall">
-          <input type="checkbox" onclick='selectall(<?php echo $nb_rows?>);' />
+          <span class="checkbox" onclick="selectall(this);">
+            <span></span>
+          </span>
         </th>
         <?php } else { ?>
-        <th style="white-space:nowrap;">
+          <th <?= !isset($_GET['monitor']) && $key == 'flag' ? 'style="padding-left: 5px;"' : '' ?>>
           <?php if ( ($SORTFIELD == $val) && ($SORTORDERFIELD == "ASC") ) { ?>
           <a class="col_sort_up" href="<?php echo $MY_GET_NO_SORT?>&sort=<?php echo $key?>&order=1">
           <?php } else if ($SORTFIELD == $val) { ?>
@@ -39,56 +56,66 @@
       </tr>
       <?php
         while($data = mysql_fetch_array($rep, MYSQL_ASSOC)) { 
-
-          if ($data['SVCST'] == 0) { $COLOR = "soft"; $soft= "soft"; }
-          else { $COLOR = ""; $soft = ""; }
           switch($data['STATUS']) {
             case 0: $COLOR = $OK; 
                     break; 
-            case 1: $COLOR = $WARNING.$COLOR; 
+            case 1: $COLOR = $WARNING; 
                     break;
-            case 2: $COLOR = $CRITICAL.$COLOR; 
+            case 2: $COLOR = $CRITICAL; 
                     break;
-            case 3: $COLOR = $UNKNOWN.$COLOR; 
+            case 3: $COLOR = $UNKNOWN; 
                     break;
-	  }
+          }
+          
           if ( ($data['ACK'] == "1") && ($LEVEL < 4) ) { 
             $COLOR = $TRACK;
           }
+          if ($data['SVCST'] == 0) { 
+            $COLOR .= " soft"; 
+          }
       ?>
-      <tr class="<?php echo $COLOR?>" id="<?php echo $data['SVCID']?>" style="font-size:<?php echo $_SESSION['FONTSIZE']?>px;"
-         onmouseover='to = setTimeout("get_data(\"<?php echo $data['TYPE']?>\", \"<?php echo $data['SVCID']?>\");",500); 
-         this.className="over<?php echo $soft?>";' 
-         onmouseout='clearTimeout(to); this.className="<?php echo $COLOR; ?>";'
-         onclick='selectline(<?php echo $line?>);'>
+      <tr class="alert-item <?php echo $COLOR?>" id="<?php echo $data['SVCID']?>"
+        <? if ($POPIN) { ?>
+          onmouseover="to = setTimeout(function() { get_data('<?= $data['TYPE'] ?>', '<?= $data['SVCID'] ?>'); }, 500);" 
+          onmouseout="clearTimeout(to);"
+        <? } ?>
+          onclick="selectline(this, event);">
         <?php 
           foreach($COLS AS $key => $val) { 
 
             if ($key == 'checkbox')
-              $toprint = '<input id="check_'.$line.'" class="chkbox" name="'.$data['SVCID'].'" value="'.$data['MACHINE_NAME'].' '.$data['SERVICES'].'" type="checkbox" onclick="selectline('.$line.');" />';
-
+              $toprint = '
+                <span class="checkbox">
+                  <input type="hidden" 
+                         class="data"
+                         name="'.$data['SVCID'].'" 
+                         value="'.$data['MACHINE_NAME'].' '.$data['SERVICES'].'" />
+                  <span></span>
+                </span>';
+              
             else if ($key == 'flag') {
 
               if ($data['TYPE'] == "svc") 
-                $toprint = '<a onclick="selectline('.$line.');" target="_BLANK" href="'.$LINK.'?type=2&host='.$data["MACHINE_NAME"].'&service='.$data["SERVICES"].'"><img src="img/svc.png" border="0" alt="S" title="'.ucfirst(lang($MYLANG, 'service')).'" /></a>'; 
+                $toprint = '<a target="_blank" href="'.$LINK.'?type=2&host='.$data["MACHINE_NAME"].'&service='.$data["SERVICES"].'"><img src="img/flag_svc.png" border="0" alt="S" title="'.ucfirst(lang($MYLANG, 'service')).'" /></a>'; 
               else
-                $toprint = '<a onclick="selectline('.$line.');" class="col_no_sort" target="_BLANK" href="'.$LINK.'?type=1&host='.$data["MACHINE_NAME"].'"><img src="img/host.png" border="0" alt="H" title="'.ucfirst(lang($MYLANG, 'host')).'" /></a>'; 
-
-              if (isset($GRAPH_POPUP)) {
-                $toprint .= '<a href="#" onClick=\'pop("graph.php?host='.$data['MACHINE_NAME'].'&svc='.$data['SERVICES'].'&period=week","'.$data['SVCID'].'",800,400);\'><img src="img/graph.png" alt="G" border="0" title="'.ucfirst(lang($MYLANG, 'graph_icon')).'" /></a>';
+                $toprint = '<a class="col_no_sort" target="_blank" href="'.$LINK.'?type=1&host='.$data["MACHINE_NAME"].'"><img src="img/flag_host.png" border="0" alt="H" title="'.ucfirst(lang($MYLANG, 'host')).'" /></a>'; 
+              
+              $g = get_graph('popup', $data['MACHINE_NAME'], $data['SERVICES']);
+              if (!is_null($g)) {
+                $toprint .= '<a href="#" onClick="return pop(\''.$g.'\', \''.$data['SVCID'].'\', 800, 400);"><img src="img/flag_graph.png" alt="G" border="0" title="'.ucfirst(lang($MYLANG, 'graph_icon')).'" /></a>';
               }
 
               if ($data['ACK'] == "1") 
-                $toprint = $toprint.'<img src="img/ack.gif" alt="A" title="'.ucfirst(lang($MYLANG, 'acknowledge')).'" />';
+                $toprint = $toprint.'<img src="img/flag_ack.gif" alt="A" title="'.ucfirst(lang($MYLANG, 'acknowledge')).'" />';
 
               if ($data['NOTIF'] == "0")
-                $toprint = $toprint.'<img src="img/notify.gif" alt="N" />';
+                $toprint = $toprint.'<img src="img/flag_notify.png" alt="N" title="'.ucfirst(lang($MYLANG, 'disa_notif')).'" />';
 
               if ($data['DOWNTIME'] == "1") 
-                $toprint = $toprint.'<img src="img/downtime.gif" alt="D" title="'.ucfirst(lang($MYLANG, 'downtime')).'" />';
+                $toprint = $toprint.'<img src="img/flag_downtime.png" alt="D" title="'.ucfirst(lang($MYLANG, 'downtime')).'" />';
 
               if ($data['COMMENT'] > 0)
-                $toprint = $toprint.'<img src="img/comment.gif" alt="C" />';
+                $toprint = $toprint.'<img src="img/flag_comment.gif" alt="C" title="'.ucfirst(lang($MYLANG, 'comment')).'" />';
 
             }
             else if ( ($key == 'duration') || ($key == 'last') ) 
@@ -112,11 +139,12 @@
             }
           if (isset($_GET['monitor'])) {
         ?>
-        <td<?php echo ($key=="flag")?" class=\"".$COLOR."dark\"":""?> style="white-space:nowrap;">
+        <td<?php echo ($key=="flag" ? " class=\"".$COLOR." dark\" style=\"text-align: left;\"":"")?>>
         <?php } else { ?>
-        <td<?php echo ($key=="checkbox")?" style=\"text-align:center;\" class=\"".$COLOR."dark\"":""?> style="white-space:nowrap;">
+        <td<?php echo ($key=="checkbox")?" class=\"".$COLOR." dark\"": ($key=="flag" ? " style=\"padding-left: 5px; text-align: left;\"":"")?>>
         <?php } ?>
-          <?php echo $toprint?>
+          <? if ($key != 'checkbox') $toprint = '<span>'.$toprint.'</span>'; ?> 
+          <?= $toprint ?>
         </td>
         <?php } //end foreach 
         ?>
@@ -126,12 +154,7 @@
         } //end while
       ?>
     </table>
-    <?php if (isset($_GET['monitor'])) { ?>
-    <?php echo ucfirst(lang($MYLANG, 'refreshing'))?> <b><span id="refreshspan"></span></b> <?php echo ucfirst(lang($MYLANG, 'second'))?>
-    <a href="index.php"><?php echo ucfirst(lang($MYLANG, 'mode0'))?></a>
-    <?php } else { ?>
-    </form>
-    <?php } ?>
+
 
 <?php /*
 if ($nb_rows > 0) {
