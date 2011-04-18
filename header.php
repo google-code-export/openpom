c6472132600814bce135711c55a013d28099e96b
omk8421 <omk8421@gmail.com>
2011-04-18 11:06:03 +0000

    Header fix for new graph popin Initialize current_data_displayed and it to null

--- header.php
+++ header.php
@@ -20,33 +20,47 @@
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta http-equiv="CACHE-CONTROL" content="NO-CACHE" />
     <meta http-equiv="PRAGMA" content="NO-CACHE" /> 
-    <link rel="StyleSheet" href="style.css" type="text/css" />
-    <script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <script type="text/javascript" src="js/jquery-1.4.4.min.js"></script>
     <script type="text/javascript" src="js/jquery.colorbox.js"></script>
     <script type='text/javascript' src="js/XMLHttpRequest-IE.js"></script>
     <script type='text/javascript' src="js/func.js"></script>
     <script type="text/javascript" src="js/scripts.js"></script>
     <script type="text/javascript">
-      var current_data_displayed;
+      var current_data_displayed = null;
       var cache    = new Array();
-      var popup;
       var refresh; 
       var mytime   = <?php echo ($REFRESHTIME+1)?>;
       var filter   = "<?php echo $FILTER?>";
       var cur_id;
-      var it;
+      var it = null;
       var filtering_has_focus = false;
       var lastChecked = null;
+      var accept_action = true;
+      
+      var popup = $('\
+        <div>\
+          <img style="margin: 20px;" src="img/loading.gif" />\
+        </div>\
+      ').css({
+        'display': 'none',
+        'background-color': 'white', 
+        'position': 'fixed', 
+        'top': '10px',
+        'right': '10px',
+        'border': '1px solid #666',
+        'border-radius': '2px',
+        '-moz-border-radius': '2px',
+        '-khtml-border-radius': '2px',
+        '-webkit-border-radius': '2px',
+        /*'behavior': 'url(\'PIE.htc\')',*/
+        'z-index': '88888'
+      });
+      
       document.onclick = hide_data;
     </script>
   </head>
   <body>
-    <div id="popup"><img src="img/ajax_loading.gif" /></div>
-    <script>
-      popup = document.getElementById("popup");
-      popup.style.visibility = "hidden";                                                  
-      document.onmousemove = WhereMouse;
-    </script>
     <?php if ($_SESSION['FRAME'] == 1) { ?>
     <table width="100%" height="100%" class="frame" id="type_<?php echo $framecolor?>">
       <tr>
