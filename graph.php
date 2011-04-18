27545095c76ada0439be3f4bb0d0e0e04ae452fb
omk8421 <omk8421@gmail.com>
2011-04-18 11:58:22 +0000

    Rebuild Nagios graph cgi wrapper

--- graph.php
+++ graph.php
@@ -13,43 +13,89 @@
 session_start();
 if (!isset($_SESSION['USER'])) die();
 require_once("config.php");
+require_once("utils.php");
 require_once("lang.php");
-$pat = '/[a-zA-Z0-9._-]{1,100}/' ;
-if ( (!preg_match($pat, $_GET['host'])) || (!preg_match($pat, $_GET['svc'])) )
-  die();
-$HOST = $_GET['host'];
-$SVC  = $_GET['svc'];
-if ($SVC == "--host--") $service = "";
-else $service = "&service=".$SVC;
-if ( (!isset($_GET['period'])) || (!preg_match('/(today|week|month|year)/',$_GET['period'])) )
-  $period = 'week';
-else $period = $_GET['period'];
+$pat = '/^[a-zA-Z0-9._-]{1,100}$/' ;
+
+
+
+/* query string: host */
+if (isset($_GET['host']) && preg_match($pat, $_GET['host'])) {
+  $host = $_GET['host'];
+} else {
+  die('Call error: host');
+}
+
+/* query string: svc */
+if (isset($_GET['svc'])) {
+  if (preg_match($pat, $_GET['svc'])) {
+    $svc = $_GET['svc'];
+  } else {
+    die('Call error: svc');
+  }
+} else {
+  $svc = null;
+}
+
+/* graph producer */
+$graph = get_graph('status', $host, $svc);
+if (is_null($graph)) {
+  die('Error: no graph target, configure GRAPH_STATUS variable');
+}
+
+/* query string: period */
+if (isset($_GET['period'])) {
+  if (!preg_match('/(day|week|month|year)/', $_GET['period'])) {
+    die('Call error: period');
+  }
+} else {
+  $_GET['period'] = 'day';
+}
+$period = $_GET['period'];
+$graph .= '&t1=' . strtotime('-1 ' . $period);
+$graph .= '&t2=' . time();
+
 ?>
 <!DCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
   <head>
-    <title><?php echo $HOST." ".$SVC; ?></title>
+  <title><?php echo $host; if (!is_null($svc)) { echo " &#160;&mdash;&#160; ".$svc; } ?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <meta http-equiv="CACHE-CONTROL" content="NO-CACHE" />
     <meta http-equiv="PRAGMA" content="NO-CACHE" />                                       
     <link rel="StyleSheet" href="style.css" type="text/css" />
-    <script type='text/javascript' src='js/func.js'></script>
+    <style type="text/css">
+    div {
+      margin: 2px;
+      margin-bottom: 20px;
+    }
+    
+    img {
+      border: 1px solid #8A8A8A;
+    }
+    
+    a.<?= $period ?> {
+      font-weight: bold;
+    }
+    </style>
   </head>
   <body>
-    <div><center>
-      <a class="graph" href="graph.php?&host=<?php echo $HOST?>&svc=<?php echo $SVC?>&period=today"><?php echo ucfirst(lang($MYLANG, 'today'))?></a>
-      <a class="graph" href="graph.php?&host=<?php echo $HOST?>&svc=<?php echo $SVC?>&period=week"><?php echo ucfirst(lang($MYLANG, 'week'))?></a>
-      <a class="graph" href="graph.php?&host=<?php echo $HOST?>&svc=<?php echo $SVC?>&period=month"><?php echo ucfirst(lang($MYLANG, 'month'))?></a>
-      <a class="graph" href="graph.php?&host=<?php echo $HOST?>&svc=<?php echo $SVC?>&period=year"><?php echo ucfirst(lang($MYLANG, 'year'))?></a>
-    </center></div>
-    <?php if ($period == 'today') { ?>
-    <img src="<?php echo $GRAPH_POPUP?>?createimage&host=<?php echo $HOST?><?php echo $service?>&backtrack=4&zoom=4&t1=<?php echo strtotime("-1 day")?>&t2=<?php echo time()?>" border="0" />
-    <?php } else if ($period == 'week') { ?>
-    <img src="<?php echo $GRAPH_POPUP?>?createimage&host=<?php echo $HOST?><?php echo $service?>&backtrack=4&zoom=4&t1=<?php echo strtotime("-1 week")?>&t2=<?php echo time()?>" border="0" />
-    <?php } else if ($period == 'month') { ?>
-    <img src="<?php echo $GRAPH_POPUP?>?createimage&host=<?php echo $HOST?><?php echo $service?>&backtrack=4&zoom=4&t1=<?php echo strtotime("-1 month")?>&t2=<?php echo time()?>" border="0" />
-    <?php } else if ($period == 'year') { ?>
-    <img src="<?php echo $GRAPH_POPUP?>?createimage&host=<?php echo $HOST?><?php echo $service?>&backtrack=4&zoom=4&t1=<?php echo strtotime("-1 year")?>&t2=<?php echo time()?>" border="0" />
-    <?php } ?>
+    <center>
+      <div>
+        <?php 
+          $uri = 'graph.php?host=' . $host;
+          if (!is_null($svc)) {
+            $uri .= '&svc=' . $svc;
+          }
+        ?>
+        
+        [ <a class="day" href="<?= $uri ?>&period=day"><?= ucfirst(lang($MYLANG, 'today')) ?></a> ]&#160;&#160; 
+        [ <a class="week" href="<?= $uri ?>&period=week"><?= ucfirst(lang($MYLANG, 'week')) ?></a> ]&#160;&#160;
+        [ <a class="month" href="<?= $uri ?>&period=month"><?= ucfirst(lang($MYLANG, 'month')) ?></a> ]&#160;&#160;
+        [ <a class="year" href="<?= $uri ?>&period=year"><?= ucfirst(lang($MYLANG, 'year')) ?></a> ]
+      </div>
+      
+      <img src="<?= $graph ?>" />
+    </center>
   </body>
 </html>
