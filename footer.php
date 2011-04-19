bf7a42ee083134ed2d67bb14fd7d7834a3deefd3
omk8421 <omk8421@gmail.com>
2011-04-19 16:37:00 +0000

    Custom ndash to separate entries on the footer

--- footer.php
+++ footer.php
@@ -13,18 +13,18 @@
   if (basename($_SERVER['SCRIPT_NAME']) != "index.php") die() ; ?>
   
   <div style="margin-top: 3px; text-align: center;">
-    <?= $CODENAME ?> &ndash; 
-    <?= $VERSION ?> &ndash; 
-    GPL &ndash;
-    <a href="http://www.exosec.fr/" target="_blank">Exosec</a> &ndash;
-    2010&thinsp;-&thinsp;2011 &ndash; 
+    <?= $CODENAME ?> <span class="ndash">&ndash;</span> 
+    <?= $VERSION ?> <span class="ndash">&ndash;</span> 
+    GPL <span class="ndash">&ndash;</span>
+    <a href="http://www.exosec.fr/" target="_blank">Exosec</a> <span class="ndash">&ndash;</span>
+    2010&thinsp;-&thinsp;2011 <span class="ndash">&ndash;</span> 
     <?= ucfirst(lang($MYLANG, 'querytime')) ?> <?= sprintf($str_query_time, $query_time) ?>
     
     <? if (isset($_GET['monitor'])) { ?>
-      &ndash; 
+      <span class="ndash">&ndash;</span> 
       <?= ucfirst(lang($MYLANG, 'refreshing')) ?>
       <b><span id="refreshspan"></span></b>&#160;<?= lang($MYLANG, 'second') ?>
-      &ndash;
+      <span class="ndash">&ndash;</span>
       <a href="index.php"><?php echo ucfirst(lang($MYLANG, 'mode0'))?></a>
     <? } ?>
   </div>
